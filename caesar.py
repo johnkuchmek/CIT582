32170e0f7f58279383ddaae2a824b097debdbc5f
zys204 <kayliesongzhenyi@gmail.com>
2021-05-17 00:28:36 -0400

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -2,39 +2,39 @@
 def encrypt(key,plaintext):
     ciphertext=""
     #YOUR CODE HERE
-	#Define Alphabet
+    #Define Alphabet
     upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-	for letter in plaintext:
-    if letter in upperCase:
-			letterPosition = upperCase.index(letter)
-			cipherPosition = (letterPosition + key) % 26
-			cipherLetter = upperCase[cipherPosition]
-			ciphertext.append(cipherLetter)
-		elif letter in lowerCase:
-			letterPosition = lowerCase.index(letter)
-			cipherPosition = (letterPosition + key) % 26
-			cipherLetter = lowerCase[cipherPosition]
-			ciphertext.append(cipherLetter)
+    for letter in plaintext:
+        if letter in upperCase:
+            letterPosition = upperCase.index(letter)
+            cipherPosition = (letterPosition + key) % 26
+            cipherLetter = upperCase[cipherPosition]
+            ciphertext.append(cipherLetter)
+        elif letter in lowerCase:
+            letterPosition = lowerCase.index(letter)
+            cipherPosition = (letterPosition + key) % 26
+            cipherLetter = lowerCase[cipherPosition]
+            ciphertext.append(cipherLetter)
     return ciphertext
 
 def decrypt(key,ciphertext):
     plaintext=""
     #YOUR CODE HERE
-	#Define Alphabet
+    #Define Alphabet
     upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-	for letter in ciphertext:
-		if letter in upperCase:
-			letterPosition = upperCase.index(letter)
-			cipherPosition = (letterPosition - key) % 26
-			plainLetter = upperCase[cipherPosition]
-			plaintext.append(plainLetter)
-		elif letter in lowerCase:
-			letterPosition = lowerCase.index(letter)
-			cipherPosition = (letterPosition + key) % 26
-			plainLetter = lowerCase[cipherPosition]
-			plaintext.append(plainLetter)
+    for letter in ciphertext:
+        if letter in upperCase:
+            letterPosition = upperCase.index(letter)
+            cipherPosition = (letterPosition - key) % 26
+            plainLetter = upperCase[cipherPosition]
+            plaintext.append(plainLetter)
+        elif letter in lowerCase:
+            letterPosition = lowerCase.index(letter)
+            cipherPosition = (letterPosition + key) % 26
+            plainLetter = lowerCase[cipherPosition]
+            plaintext.append(plainLetter)
     return plaintext
 
 
