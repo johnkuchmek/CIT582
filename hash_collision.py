123bf799eea13c01c85631dae300bc51232a267b
zys204 <kayliesongzhenyi@gmail.com>
2021-05-24 19:22:17 -0400

    Update hash_collision.py

--- hash_collision.py
+++ hash_collision.py
@@ -12,27 +12,27 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
    
     #Collision finding code goes here
-	letters = string.ascii_letters
-	strX = ''.join(random.choice(letters) for i in range (256))
-	x = strX.encode('utf-8')
-	strY = ''.join(random.choice(letters) for i in range (256))
-	y = strY.encode('utf-8')
-	
-	isCollision = 0
-	
-	while (isCollision = 0):
-		hashX = hashlib.sha256(x).digest()
-		hashY = hashlib.sha256(y).digest()
-		intX = int.from_bytes(hashX)
-		intY = int.from_bytes(hashY)
-		binX = bin(intX)
-		binY = bin(intY)
-		if (binX[(len(binX)-k):] == binY[(len(binY)-k):]):
-			isCollision = 1
-		else:
-			strY = ''.join(random.choice(letters) for i in range (256))
-			y = strY.encode('utf-8')
-	
+    letters = string.ascii_letters
+    strX = ''.join(random.choice(letters) for i in range (256))
+    x = strX.encode('utf-8')
+    strY = ''.join(random.choice(letters) for i in range (256))
+    y = strY.encode('utf-8')
+    
+    isCollision = 0
+    
+    while (isCollision = 0):
+        hashX = hashlib.sha256(x).digest()
+        hashY = hashlib.sha256(y).digest()
+        intX = int.from_bytes(hashX)
+        intY = int.from_bytes(hashY)
+        binX = bin(intX)
+        binY = bin(intY)
+        if (binX[(len(binX)-k):] == binY[(len(binY)-k):]):
+            isCollision = 1
+        else:
+            strY = ''.join(random.choice(letters) for i in range (256))
+            y = strY.encode('utf-8')
+    
     #x = b'\x00'
     #y = b'\x00'
     
