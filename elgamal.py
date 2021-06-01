24e8966f82aba5bd90f03458024c98dac079f58f
zys204 <kayliesongzhenyi@gmail.com>
2021-06-01 12:46:16 -0400

    El Gamal Commit
    El Gamal commit

---
+++ elgamal.py
@@ -0,0 +1,26 @@
+import random
+
+from params import p
+from params import g
+
+def keygen():
+    rand = random.seed(a=None, version = 2)
+    q = (p-1)/2
+    a = random.randint(1,q)
+    sk = a
+    pk = pow(g,a,p)
+    return pk,sk
+
+def encrypt(pk,m):
+    rand = random.seed(a=None, version = 2)
+    r = random.randint(1,q)
+    c1 = pow(g,r,p)
+    c2 = pk * pow(m,1,p)
+    return [c1,c2]
+
+def decrypt(sk,c):
+    c1 = c[1]
+    c2 = c[2]
+    m = pow(c2/c1^a,1,p)
+    return m
+
