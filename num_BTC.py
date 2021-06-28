32cb32be92d4b9f6ecb9fda0f828913a25bd8ce7
zys204 <kayliesongzhenyi@gmail.com>
2021-06-27 23:30:00 -0400

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -3,7 +3,7 @@ import math
 def num_BTC(b):
     currentBlock = 1
     currentReward = float(50)
-    c = float(0)
+    c = float(50)
     while currentBlock != b:
         if currentBlock % 210000 == 0:
             currentBlock = currentBlock + 1
