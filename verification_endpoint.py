4cf947eeb1b3f91c57fea3fc716115932f9caa9a
zys204 <kayliesongzhenyi@gmail.com>
2021-07-11 15:52:26 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -42,7 +42,7 @@ def verify():
 			# result = False
 
     # else:
-        result = False #Should only be true if signature validates
+    result = False #Should only be true if signature validates
     
     return jsonify(result)
 
