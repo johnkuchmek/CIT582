32ffd68b6f7558ad9363c07f54587d0eeae9d9a8
zys204 <kayliesongzhenyi@gmail.com>
2021-07-11 16:46:02 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -25,7 +25,7 @@ def verify():
 
         payload = json.dumps(content['payload'])
         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-		eth_sig_obj = content['sig']
+        eth_sig_obj = content['sig']
         
         if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
             result = True
