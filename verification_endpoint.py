051a337ca1ccce8ea6cc77eef506f1a18f269d35
zys204 <kayliesongzhenyi@gmail.com>
2021-07-11 15:51:24 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,7 +11,7 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    content = json.loads(content)
+    dictContent = json.loads(content)
     #Check if signature is valid
     
     if dictContent['payload']['platform'] = "Ethereum":
@@ -21,14 +21,14 @@ def verify():
         eth_pk = acct.address
         eth_sk = acct.key
 
-        payload = content['payload']
+        payload = dictContent['payload']
         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
         eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
         
-        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
-            result = False
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == dictContent['payload']['pk']:
+            result = True
 		else:
-			result = True
+			result = False
         
     elif dictContent['payload']['platform'] = "Algorand":
         payload = content['payload']
@@ -36,10 +36,10 @@ def verify():
         algo_sk, algo_pk = algosdk.account.generate_account()
         algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
 
-        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
-            result = False
+        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,dictContent['payload']['pk']):
+            result = True
 		else:
-			result = True
+			result = False
 
     else:
         result = False #Should only be true if signature validates
