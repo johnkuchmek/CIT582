5c035fb5e5bdb3c807d1a3a98a1e374958fa1bdf
zys204 <kayliesongzhenyi@gmail.com>
2021-07-11 15:52:02 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -14,34 +14,34 @@ def verify():
     dictContent = json.loads(content)
     #Check if signature is valid
     
-    if dictContent['payload']['platform'] = "Ethereum":
-        eth_account.Account.enable_unaudited_hdwallet_features()
-        acct, mnemonic = eth_account.Account.create_with_mnemonic()
+    # if dictContent['payload']['platform'] = "Ethereum":
+        # eth_account.Account.enable_unaudited_hdwallet_features()
+        # acct, mnemonic = eth_account.Account.create_with_mnemonic()
         
-        eth_pk = acct.address
-        eth_sk = acct.key
+        # eth_pk = acct.address
+        # eth_sk = acct.key
 
-        payload = dictContent['payload']
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
+        # payload = dictContent['payload']
+        # eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        # eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
         
-        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == dictContent['payload']['pk']:
-            result = True
-		else:
-			result = False
+        # if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == dictContent['payload']['pk']:
+            # result = True
+		# else:
+			# result = False
         
-    elif dictContent['payload']['platform'] = "Algorand":
-        payload = content['payload']
+    # elif dictContent['payload']['platform'] = "Algorand":
+        # payload = content['payload']
         
-        algo_sk, algo_pk = algosdk.account.generate_account()
-        algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
+        # algo_sk, algo_pk = algosdk.account.generate_account()
+        # algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
 
-        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,dictContent['payload']['pk']):
-            result = True
-		else:
-			result = False
+        # if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,dictContent['payload']['pk']):
+            # result = True
+		# else:
+			# result = False
 
-    else:
+    # else:
         result = False #Should only be true if signature validates
     
     return jsonify(result)
