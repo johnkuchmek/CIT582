eead8e447944e0291d60a6ee91ff5baf2ee8febb
zys204 <kayliesongzhenyi@gmail.com>
2021-07-11 16:40:00 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -38,7 +38,7 @@ def verify():
         algo_sk, algo_pk = algosdk.account.generate_account()
         algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
 
-        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
+        if algosdk.util.verify_bytes(payload.encode('utf-8'),content['sig'],content['payload']['pk']):
             result = True
         else:
             result = False
