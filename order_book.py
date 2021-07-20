f73a44dd9dee89fb6ad2913bfc8e16b1097bc238
zys204 <kayliesongzhenyi@gmail.com>
2021-07-19 22:24:34 -0400

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -32,33 +32,34 @@ def process_order(order):
     for orderToMatch in session.query(Order):
         possibleOrder = session.query(Order).filter(Order.filled == None, Order.buy_currency == orderToMatch.sell_currency, Order.sell_currency == orderToMatch.buy_currency,\
         (Order.sell_amount / Order.buy_amount) >= (orderToMatch.buy_amount / orderToMatch.sell_amount)).first()
-  
-        orderToMatch.timestamp = datetime.now()
-        possibleOrder.timestamp = datetime.now()
-        orderToMatch.counterparty_id = possibleOrder.id
-        possibleOrder.counterparty_id = orderToMatch.id
-        if orderToMatch.buy_amount > possibleOrder.sell_amount or orderToMatch.sell_amount > possibleOrder.buy_amount:
-            newOrder['sender_pk'] = orderToMatch.sender_pk
-            newOrder['receiver_pk'] = orderToMatch.receiver_pk
-            newOrder['buy_currency'] = orderToMatch.buy_currency
-            newOrder['sell_currency'] = orderToMatch.sell_currency
-            newOrder['buy_amount'] = orderToMatch.buy_amount - possibleOrder.sell_amount
-            newOrder['sell_amount'] = orderToMatch.sell_amount - possibleOrder.buy_amount
-            newOrder['creator_id'] = orderToMatch.id
-        elif orderToMatch.buy_amount < possibleOrder.sell_amount or orderToMatch.sell_amount < possibleOrder.buy_amount:
-            newOrder['sender_pk'] = possibleOrder.sender_pk
-            newOrder['receiver_pk'] = possibleOrder.receiver_pk
-            newOrder['buy_currency'] = possibleOrder.buy_currency
-            newOrder['sell_currency'] = possibleOrder.sell_currency
-            newOrder['buy_amount'] = possibleOrder.buy_amount - orderToMatch.sell_amount
-            newOrder['sell_amount'] = possibleOrder.sell_amount - orderToMatch.buy_amount
-            newOrder['creator_id'] = possibleOrder.id
-        
-        fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount']
-        order_obj = Order(**{f:order[f] for f in fields})
+		
+		if possibleOrder != None:
+			orderToMatch.timestamp = datetime.now()
+			possibleOrder.timestamp = datetime.now()
+			orderToMatch.counterparty_id = possibleOrder.id
+			possibleOrder.counterparty_id = orderToMatch.id
+			if orderToMatch.buy_amount > possibleOrder.sell_amount or orderToMatch.sell_amount > possibleOrder.buy_amount:
+				newOrder['sender_pk'] = orderToMatch.sender_pk
+				newOrder['receiver_pk'] = orderToMatch.receiver_pk
+				newOrder['buy_currency'] = orderToMatch.buy_currency
+				newOrder['sell_currency'] = orderToMatch.sell_currency
+				newOrder['buy_amount'] = orderToMatch.buy_amount - possibleOrder.sell_amount
+				newOrder['sell_amount'] = orderToMatch.sell_amount - possibleOrder.buy_amount
+				newOrder['creator_id'] = orderToMatch.id
+			elif orderToMatch.buy_amount < possibleOrder.sell_amount or orderToMatch.sell_amount < possibleOrder.buy_amount:
+				newOrder['sender_pk'] = possibleOrder.sender_pk
+				newOrder['receiver_pk'] = possibleOrder.receiver_pk
+				newOrder['buy_currency'] = possibleOrder.buy_currency
+				newOrder['sell_currency'] = possibleOrder.sell_currency
+				newOrder['buy_amount'] = possibleOrder.buy_amount - orderToMatch.sell_amount
+				newOrder['sell_amount'] = possibleOrder.sell_amount - orderToMatch.buy_amount
+				newOrder['creator_id'] = possibleOrder.id
+			
+			fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount']
+			order_obj = Order(**{f:order[f] for f in fields})
 
-        session.add(order_obj)
-        session.commit()
+			session.add(order_obj)
+			session.commit()
     
     pass
   
