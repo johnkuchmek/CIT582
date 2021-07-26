3fdd9600bac2df9e9f0d3d31cd756a22995a120e
zys204 <kayliesongzhenyi@gmail.com>
2021-07-25 21:51:24 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -6,7 +6,7 @@ def ZK_equality(G,H):
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
     r1 = random.seed(a=None, version = 2)
     r2 = random.seed(a=None, version = 2)
-    m = random.seed(a=None, version = 2)
+    m = "I am an Yipu"
     
     C1 = r1 * G
     C2 = r1 * H + m * G
@@ -14,7 +14,7 @@ def ZK_equality(G,H):
     D2 = r2 * H + m * G
 
     #Generate a NIZK proving equality of the plaintexts
-    stmt = DLRep(C1,r1*G) | DLRep(C2,r1*H+m*G) | DLRep(D1,r2*G) | DLRep(D2,r2*H+m*G)
+    stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
     zk_proof = stmt.prove()
 
     #Return two ciphertexts and the proof
