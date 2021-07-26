004987b42eb8dda5ce92ae425aa109a7d5e7c992
zys204 <kayliesongzhenyi@gmail.com>
2021-07-25 22:40:02 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -4,8 +4,8 @@ from zksk import utils
 def ZK_equality(G,H):
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
-    r1 = Secret(utils.get_random_num(bits=32))
-    r2 = Secret(utils.get_random_num(bits=32))
+    r1 = utils.get_random_num(bits=32)
+    r2 = utils.get_random_num(bits=32)
     m = utils.get_random_num(bits=32)
 
     C1 = (r1 * G)
