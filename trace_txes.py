ce217382c4da30a689f8b16df98869bb8903259d
zys204 <kayliesongzhenyi@gmail.com>
2021-08-02 14:25:30 -0400

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -55,10 +55,10 @@ class TXO:
         print("I am being tested")
         if d > 0:
             tx = rpc_connection.getrawtransaction(self.tx_hash,True)
-            for input in tx['vin']:
-                input_obj = TXO()
-                input_obj = from_tx_hash(input_obj, input['txid'],input['vout'])
-                self.inputs.append(input_obj)
-                if (d > 1):
-                    input_obj.get_inputs(input_obj, d-1)
+            #for input in tx['vin']:
+            #    input_obj = TXO()
+            #    input_obj = from_tx_hash(input_obj, input['txid'],input['vout'])
+            #    self.inputs.append(input_obj)
+            #    if (d > 1):
+            #        input_obj.get_inputs(input_obj, d-1)
         
