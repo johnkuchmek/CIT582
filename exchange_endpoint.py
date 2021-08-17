333f5c7252ad17877594dc24ada08ececae3ce6d
zys204 <kayliesongzhenyi@gmail.com>
2021-08-16 20:19:17 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -238,14 +238,71 @@ def trade():
         # 3a. Check if the order is backed by a transaction equal to the sell_amount (this is new)
 		if content['payload']['platform'] == "Ethereum":
 			tx = w3.eth.get_transaction(content['payload']['tx_id'])
-			
-			
+			if tx = []:
+				return jsonify(False)
+		elif content['payload']['platform'] == "Algorand":
+			tx = algosdk.search_transactions(txid=content['payload']['tx_id'])
+			if tx = []:
+				return jsonify(False)
 
 
         # 3b. Fill the order (as in Exchange Server II) if the order is valid
+        for orderToMatch in g.session.query(Order).filter(Order.filled == None):
+        possibleOrder = g.session.query(Order).filter(Order.filled == None, Order.buy_currency == orderToMatch.sell_currency, Order.sell_currency == orderToMatch.buy_currency,\
+        (Order.sell_amount / Order.buy_amount) >= (orderToMatch.buy_amount / orderToMatch.sell_amount)).first()
         
+        if possibleOrder != None and orderToMatch.filled == None:
+            orderToMatch.filled = datetime.now()
+            orderToMatch.counterparty_id = possibleOrder.id
+            g.session.commit()
+			execute_txes(orderToMatch, orderToMatch.sell_currency)
+            
+            possibleOrder.filled = datetime.now()
+            possibleOrder.counterparty_id = orderToMatch.id
+            g.session.commit()
+			execute_txes(orderToMatch, orderToMatch.sell_currency)
+            
+            if orderToMatch.buy_amount > possibleOrder.sell_amount or orderToMatch.sell_amount > possibleOrder.buy_amount:
+                newOrder = {}
+                newOrder['sender_pk'] = orderToMatch.sender_pk
+                newOrder['receiver_pk'] = orderToMatch.receiver_pk
+                newOrder['buy_currency'] = orderToMatch.buy_currency
+                newOrder['sell_currency'] = orderToMatch.sell_currency
+                if orderToMatch.buy_amount > possibleOrder.sell_amount:
+                    newOrder['buy_amount'] = orderToMatch.buy_amount - possibleOrder.sell_amount
+                    newOrder['sell_amount'] = orderToMatch.sell_amount / orderToMatch.buy_amount * newOrder['buy_amount']
+                elif orderToMatch.sell_amount > possibleOrder.buy_amount:
+                    newOrder['sell_amount'] = orderToMatch.sell_amount - possibleOrder.buy_amount
+                    newOrder['buy_amount'] = orderToMatch.buy_amount / orderToMatch.sell_amount * newOrder['sell_amount']
+                newOrder['creator_id'] = orderToMatch.id
+                
+                fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount','creator_id']
+                order_obj = Order(**{f:newOrder[f] for f in fields})
+                g.session.add(order_obj)
+                g.session.commit()
+
+            if orderToMatch.buy_amount < possibleOrder.sell_amount or orderToMatch.sell_amount < possibleOrder.buy_amount:
+                newOrder = {}
+                newOrder['sender_pk'] = possibleOrder.sender_pk
+                newOrder['receiver_pk'] = possibleOrder.receiver_pk
+                newOrder['buy_currency'] = possibleOrder.buy_currency
+                newOrder['sell_currency'] = possibleOrder.sell_currency
+                if possibleOrder.sell_amount > orderToMatch.buy_amount:
+                    newOrder['sell_amount'] = possibleOrder.sell_amount - possibleOrder.buy_amount
+                    newOrder['buy_amount'] = possibleOrder.buy_amount / possibleOrder.sell_amount * newOrder['sell_amount']
+                elif possibleOrder.buy_amount > orderToMatch.sell_amount:
+                    newOrder['buy_amount'] = possibleOrder.buy_amount - possibleOrder.sell_amount
+                    newOrder['sell_amount'] = possibleOrder.sell_amount / possibleOrder.buy_amount * newOrder['buy_amount']
+                newOrder['creator_id'] = possibleOrder.id
+                
+                fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount', 'creator_id']
+                order_obj = Order(**{f:newOrder[f] for f in fields})
+                g.session.add(order_obj)
+                g.session.commit()
+				
         # 4. Execute the transactions
         
+		
         # If all goes well, return jsonify(True). else return jsonify(False)
         return jsonify(True)
 
