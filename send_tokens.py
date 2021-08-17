f59ad0fbbd6f249b306eea02ff45d522816cf31b
zys204 <kayliesongzhenyi@gmail.com>
2021-08-17 16:24:00 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -12,7 +12,7 @@ def connect_to_algo(connection_type=''):
     if connection_type == "indexer":
         # TODO: return an instance of the v2client indexer. This is used for checking payments for tx_id's
         algod_address = "https://testnet-algorand.api.purestake.io/idx2"
-        client = indexer.IndexerClient(indexer_token=algod_token, indexer_address=algod_address)
+        client = indexer.IndexerClient(algod_token, algod_address)
         return client
     else:
         # TODO: return an instance of the client for sending transactions
