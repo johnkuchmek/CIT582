bf3b0f63057659e43c28b8b4ac28529f03c1efa9
zys204 <kayliesongzhenyi@gmail.com>
2021-08-17 15:18:11 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -236,6 +236,13 @@ def trade():
         
         # 2. Add the order to the table
         orderFields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount', 'creator_id']
+        newOrder = {}
+        newOrder['sender_pk'] = content["payloads"]["sender_pk"]
+        newOrder['receiver_pk'] = content["payloads"]["receiver_pk"]
+        newOrder['buy_currency'] = content["payloads"]["buy_currency"]
+        newOrder['sell_currency'] = content["payloads"]["sell_currency"]
+        newOrder['buy_amount'] = content["payloads"]["buy_amount"]
+        newOrder['sell_amount'] = content["payloads"]["sell_amount"]
         order_obj = Order(**{f:newOrder[f] for f in orderFields})
         g.session.add(order_obj)
         g.session.commit
