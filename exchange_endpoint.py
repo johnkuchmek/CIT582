670fb4d098bc67ddde8d0843911ed6d0be1eeef6
zys204 <kayliesongzhenyi@gmail.com>
2021-08-17 14:55:21 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -157,9 +157,10 @@ def address():
     print("I am trying")
     if request.method == "POST":
         content = request.get_json(silent=True)
-        #if 'sell_currency' not in content['payload'].keys():
-        #    print( f"Error: no platform provided" )
-        #    return jsonify( "Error: no platform provided" )
+        print(content)
+        if 'sell_currency' not in content['payload'].keys():
+            print( f"Error: no platform provided" )
+            return jsonify( "Error: no platform provided" )
         if not content['payload']['sell_currency'] in ["Ethereum", "Algorand"]:
             print( f"Error: {content['payload']['sell_currency']} is an invalid platform" )
             return jsonify( f"Error: invalid platform provided: {content['payload']['sell_currency']}"  )
